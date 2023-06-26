1779fb43ab1a70eca7082c7c846b8064eb969dd6
ArnT81 <anders_soderberg@hotmail.com>
2023-06-26 11:20:42 +0200

    dotenv for server certificates

--- imageOfMe.js
+++ imageOfMe.js
@@ -1,10 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+};
 const express = require('express');
 const app = express();
 const port = 4002;
+const { SSL_KEY, SSL_CERT } = process.env;
+
 
 app.get('/', (req, res) => {
 	res.sendFile(`${__dirname}/media/me.png`)
-})
+});
 
 if (__dirname.includes('Desktop')) {
 	app.listen(port, console.log(`Server started on port ${port}, serving an image of me`));
@@ -13,6 +18,6 @@ else {
 	const sslServer = https.createServer({
 		key: fs.readFileSync(SSL_KEY),
 		cert: fs.readFileSync(SSL_CERT),
-	}, app)
+	}, app);
 	sslServer.listen(port, () => console.log(`Secure server started on port ${port}, serving an image of me`));
-}
+};
